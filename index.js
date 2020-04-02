76ef961342c759bd4807fe32f0487006b3f4a6d6
root <gsshaurya@gmail.com>
2020-04-03 02:20:56 +0530

    testing

--- index.js
+++ index.js
@@ -15,78 +15,77 @@ app.use(express.json())
 // app.use(morgan('tiny'))
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body1'))
 
-let contacts = [
-      {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      },
-      {
-        "name": "asw",
-        "id": 7,
-        "number": "12"
-      }
-    ]
+// let contacts = [
+//       {
+//         "name": "Arto Hellas",
+//         "number": "040-123456",
+//         "id": 1
+//       },
+//       {
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523",
+//         "id": 2
+//       },
+//       {
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345",
+//         "id": 3
+//       },
+//       {
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122",
+//         "id": 4
+//       },
+//       {
+//         "name": "asw",
+//         "id": 7,
+//         "number": "12"
+//       }
+//     ]
 
-app.get('/api/persons',(req,res) => {
+app.get('/api/persons',(req,res,next) => {
     Person.find({})
     .then(response => {
         res.json(response.map(ele => ele.toJSON()))
     })
-    // res.send(contacts)
+    .catch(err => next(err))
 })
 
-app.get('/info',(req,res)=> {
+app.get('/info',(req,res,next)=> {
     const date = new Date()
-    res.send(`<p>Phonebook has ${contacts.length} people</p><p>${date}</p>`)
+    Person.find({})
+    .then(resp => res.send(`<p>Phonebook has ${resp.length} people</p><p>${date}</p>`))
+    .catch(err => next(err))
 })
 
-app.get('/api/persons/:id',(req,res) => {
-    const id = Number(req.params.id)
-    const person = contacts.find((ele) => ele.id === id)
-    console.log(person)
-    if(!person){
-    res.status(404).json({
-        error: "person not found"
-    })
-    }
-    else{
-        res.send(person)
-    }
-})
-
-app.delete('/api/persons/:id', (req,res) => {
+app.get('/api/persons/:id',(req,res,next) => {
     const id = req.params.id
     // const person = contacts.find((ele) => ele.id === id)
-    // if(person){
-    // contacts = contacts.filter(contact => contact.id !== id)
-    // res.status(204).send('person deleted')
+    // console.log(person)
+    // if(!person){
+    // res.status(404).json({
+    //     error: "person not found"
+    // })
     // }
-    // else
-    //     res.send('person not found').status(404)
-    console.log(id)
+    // else{
+        
+    // }
+    Person.find({_id:id})
+    .then(resp => res.send(resp))
+    .catch(err => next(err))
+
+})
+
+app.delete('/api/persons/:id', (req,res,next) => {
+    const id = req.params.id
+    
     Person.findByIdAndDelete(id)
     .then(resp => {console.log(resp)
         res.status(204).send('person deleted')})
     .catch(err => next(err))
 })
 
-app.post('/api/persons',(req,res) => {
+app.post('/api/persons',(req,res,next) => {
     const body = req.body
     if(!body.name){
         return res.status(400).json({
@@ -101,14 +100,23 @@ app.post('/api/persons',(req,res) => {
     const newPerson = new Person({
         name: body.name,
         number:body.number
-        // id: Math.floor(Math.random()*500000)
     })
-    contacts = [...contacts,newPerson]
-    // res.send(newPerson)
     newPerson.save()
     .then(response => res.json(response.toJSON()))
+    .catch(err => next(err))
 })
 
+
+app.put('/api/persons/:id',(req,res,next) => {
+    const id = req.params.id
+    const body = req.body
+    Person.findOneAndUpdate({_id: id} ,{$set: {number:body.number}} )
+    .then(resp => {
+        console.log(resp)
+        res.send(resp)
+    })
+    .catch(err => next(err))
+})
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
